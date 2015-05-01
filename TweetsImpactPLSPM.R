1683b6c96ba18b7f99ef20b0b4a3df93522a3e54
Manuel de la Torre <manuelmhtr@gmail.com>
2015-05-01 10:56:50 -0500

    Research

--- TweetsImpactPLSPM.R
+++ TweetsImpactPLSPM.R
@@ -4,12 +4,14 @@
 # Load libraries
 library("plspm")
 library("plsdepot")
+library("corrplot")
 
 # Load data
-setwd("/Users/manuelmhtr/Projects/Learning-R/ViralImpact");
+setwd("/Users/manuelmhtr/Projects/TweetsImpactPrediction/PLSPM");
 list.files("./data")
-tweetsRaw = read.csv("./data/tweetsSummary_v3.2withMozScore.csv")
+tweetsRaw = read.csv("./data/tweetsSummary_v4.0newSet.csv")
 head(tweetsRaw)
+dim(tweetsRaw)
 
 # Filter useless data
 # tweets = subset(tweetsRaw, select=-c(id, userIdStr, twitterIdStr, messageIsDirect, postHourOfDay, postDayOfWeek))
@@ -28,20 +30,55 @@ tweets$userListedRatio         = 1 - 1 / ((tweetsRaw$userListedCount / 500) + 1)
 tweets$userVerified            = tweetsRaw$userVerified
 tweets$id = NULL
 
+# Custom normalization
+tweets = subset(tweetsRaw, select=c(id))
+tweets$messageReach            = (tweetsRaw$messageIsDirect) * tweetsRaw$messageMentionsCount + (1 - tweetsRaw$messageIsDirect) * (tweetsRaw$userFollowersCount) + 1
+tweets$messageReachRatio       = customNormalizationMean(tweets$messageReach)
+tweets$clicksRatio             = customNormalizationMean(tweetsRaw$clicksCount) 
+tweets$retweetsRatio           = customNormalizationMean(tweetsRaw$retweetsCount)
+tweets$favoritesRatio          = customNormalizationMean(tweetsRaw$favoritesCount) 
+tweets$userKloutLevel          = tweetsRaw$userKloutScore / 100
+tweets$userMozLevel            = tweetsRaw$userMozScore   / 100
+tweets$messageHasMedia         = tweetsRaw$messageHasMedia
+tweets$userFollowersRatio      = customNormalizationMean(tweetsRaw$userFollowersCount)
+tweets$userListedRatio         = customNormalizationSd(tweetsRaw$userListedCount)
+tweets$userVerified            = tweetsRaw$userVerified
+tweets$id = NULL
+
+
+hist(tweets$messageReachRatio)
+hist(tweets$clicksRatio)
+hist(tweets$retweetsRatio)
+hist(tweets$favoritesRatio)
+hist(tweets$userFollowersRatio)
+hist(tweets$userListedRatio)
+
 summary(tweets);
+M <- cor(tweets)
+corrplot.mixed(M, tl.pos="lt")
+?corrplot.mixed
 
 # Tests for normalizagin data
-originalVar = tweetsRaw$retweetsCount
+originalVar = tweets$messageReach
 summary(originalVar);
+sd(originalVar)
 boxplot(originalVar)
 hist(originalVar)
 
-checkingVar = 1 - 1 / ((1 * originalVar / 10) + 1) 
-summary(checkingVar);
+checkingVar = 1 - 1 / ((1 * originalVar / (sd(originalVar) ^ 0.7)) + 1) 
+#checkingVar = 1 - 1 / ((originalVar / 10000) + 1)
+summary(checkingVar)
+sd(checkingVar)
 boxplot(checkingVar)
 hist(checkingVar, breaks=20)
 
-hist(tweets$messageReachRatio)
+customNormalizationMean = function(original) {
+  return(1 - 1 / (((original) / (mean(originalVar))) + 1))
+}
+
+customNormalizationSd = function(original) {
+  return(1 - 1 / ((original / sd(originalVar)) + 1))
+}
 
 # Display data
 head(tweets, n=6)
@@ -157,3 +194,50 @@ plot(bootPls, what="weights")
 
 # Bootstraping results
 bootPls$boot
+
+
+
+# Plot Neural Network Error
+library("RMySQL");
+db <- dbConnect(RMySQL::MySQL(), user='root', password='', dbname='viral_development', host='127.0.0.1')
+#dbSendQuery(db, "UPDATE stats_raw set predictedClicksRatio = NULL, predictedRetweetsRatio = NULL, predictedFavoritesRatio = NULL WHERE 1");
+
+# Control test
+result <- dbSendQuery(db, "SELECT actualRetweetsRatio, actualFavoritesRatio, actualClicksRatio FROM stats_raw WHERE 1 LIMIT 100000");
+controlData = dbFetch(result, n=-1)
+controlData$avgImpact = controlData$actualRetweetsRatio + controlData$actualFavoritesRatio + controlData$actualClicksRatio
+meanAvgImpact = mean(controlData$avgImpact)
+controlData$stdError  = (controlData$avgImpact - meanAvgImpact)
+
+# Regular Neural network test
+result <- dbSendQuery(db, "SELECT id, predictionError FROM stats_raw WHERE predictionError IS NOT NULL LIMIT 100000");
+errorData = dbFetch(result, n=-1)
+
+# Square error
+#errors = controlData$stdError^2;
+errors = errorData$predictionError^2;
+
+# Init vars
+data = c();
+
+# Build linear model
+errorsLm = lm(errors ~ c(1:length(errors)))
+data$coefA = errorsLm$coefficients[[1]];
+data$coefB = errorsLm$coefficients[[2]];
+
+# Show summary
+summary(errors)
+data$totalError = sum(errors);
+data$meanError  = mean(errors);
+data$maxError   = max(errors);
+data$minError   = min(errors);
+data$success    = length(errors[errors < 0.25]);
+data$successPer = data$success / length(errors);
+
+# Plot data and linear model
+#par(mfrow=c(1,1), mar=c(1,1,1,1))
+plot.ts(errors, type="c");
+abline(errorsLm, col="red");
+
+# Finally, print data
+print(data);
